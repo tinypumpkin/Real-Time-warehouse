416bb225379336ac98d61d2263b0eec51fd627d7
tinypumpkin <760702659@qq.com>
2020-06-16 21:55:53 +0800

    up3

--- produ.sh
+++ produ.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 JAVA_BIN=/opt/module/jdk1.8.0_212/bin/java
 APPNAME=gmall2020-mock-log-2020-05-10.jar
-SERVER_PORT=8081
  
 case $1 in
  "start")
@@ -10,7 +9,7 @@ case $1 in
     for i in hadoop100 
     do
      echo "======== $i 开启数据生成==============="
-    ssh $i  "$JAVA_BIN -Xms32m -Xmx64m  -jar /opt/module/applog/$APPNAME --server.port=$SERVER_PORT >/dev/null 2>&1  &"
+    ssh $i  "$JAVA_BIN -Xms32m -Xmx64m  -jar /opt/module/applog/$APPNAME >/dev/null 2>&1  &"
     done
   };;
   "stop")
