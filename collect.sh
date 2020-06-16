3395c0b8ec54632d2c3c488f54ca6ee9e1eeee55
tinypumpkin <760702659@qq.com>
2020-06-16 19:22:26 +0800

    备份

--- collect.sh
+++ collect.sh
@@ -1,30 +1,25 @@
-#!/bin/bash
-JAVA_BIN=/opt/module/jdk1.8.0_212/bin/java
-PROJECT=realtime-warehouse
+#! /bin/bash
+
+PROJECT=opt/module/realtime-warehouse
 APPNAME=gmall0105-logger-0.0.1-SNAPSHOT.jar
 SERVER_PORT=8081
- 
 case $1 in
- "start")
-   {
-     echo "========NGINX==============="
-    /opt/module/nginx/sbin/nginx
- 
-    for i in hadoop100 hadoop101 hadoop102
-    do
-     echo "========: $i==============="
-    ssh $i  "$JAVA_BIN -Xms32m -Xmx64m  -jar /$PROJECT/$APPNAME --server.port=$SERVER_PORT >/dev/null 2>&1  &"
-    done
-  };;
-  "stop")
-  { 
-    for i in  hadoop102 hadoop101 hadoop100
-    do
-     echo "========: $i==============="
+"start"){
+  echo "========NGINX==============="
+  /opt/module/nginx/sbin/nginx
+	for i in hadoop100 hadoop101 hadoop102 
+	do
+  echo "========$i 开启收集队列==============="
+  ssh $i "java -Xms32m -Xmx64m  -jar /$PROJECT/$APPNAME --server.port=$SERVER_PORT >/dev/null 2>&1  &"
+	done
+};;
+"stop"){
+	for i in hadoop100 hadoop101 hadoop102
+	do
+     echo "========$i 关闭收集队列==============="
      ssh $i "ps -ef|grep $APPNAME |grep -v grep|awk '{print \$2}'|xargs kill" >/dev/null 2>&1
-    done
-     echo "======== NGINX==============="
-    /opt/module/nginx/sbin/nginx  -s stop
- 
-  };;
-   esac
+	done
+  echo "======== NGINX==============="
+  /opt/module/nginx/sbin/nginx  -s stop
+};;
+esac
